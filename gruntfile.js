9d1897e24c1d0415d87b6b4742a5cfa0b4b98b60
bencoveney <bencoveney@gmail.com>
2016-03-03 22:00:42 +0000

    Addressed various CodeClimate issues

--- gruntfile.js
+++ gruntfile.js
@@ -1,85 +1,85 @@
 module.exports = function ( grunt ) {
-	'use strict';
+    'use strict';
 
-	// Define the configuration
-	grunt.initConfig({
-	
-		// Watch files and process the above tasks
-		watch: {
-			options: {
-				livereload: false
-			},
-			grunt: {
-				files: [
-					'Gruntfile.js'
-				],
-				options: {
-					reload: true
-				}
-			},
-			sass: {
-				files: [
-					'src/**/*.scss'
-				],
-				tasks: [
-					'sass',
-					'autoprefixer'
-				]
-			}
-		},
-		
-		// Clear files and folders
-		clean: {
-			all: [ 'dist' ]
-		},
-		
-		// Compile Sass files to CSS
-		sass: {
-			options: {
-				sourceMap: true,
-				outputStyle: 'compressed'
-			},
-			dev: {
-				files: {
-					'dist/Milligrid.css': 'src/**/Milligrid.scss',
-					'dist/MilligridExtended.css': 'src/**/MilligridExtended.scss'
-				}
-			}
-		},
+    // Define the configuration
+    grunt.initConfig({
+    
+        // Watch files and process the above tasks
+        watch: {
+            options: {
+                livereload: false
+            },
+            grunt: {
+                files: [
+                    'Gruntfile.js'
+                ],
+                options: {
+                    reload: true
+                }
+            },
+            sass: {
+                files: [
+                    'src/**/*.scss'
+                ],
+                tasks: [
+                    'sass',
+                    'autoprefixer'
+                ]
+            }
+        },
+        
+        // Clear files and folders
+        clean: {
+            all: [ 'dist' ]
+        },
+        
+        // Compile Sass files to CSS
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'compressed'
+            },
+            dev: {
+                files: {
+                    'dist/Milligrid.css': 'src/**/Milligrid.scss',
+                    'dist/MilligridExtended.css': 'src/**/MilligridExtended.scss'
+                }
+            }
+        },
 
-		// Parse CSS and add vendor-prefixed CSS properties using the Can I Use database.
-		autoprefixer: {
-			default: {
-				options: {
-					browsers: [
-						'last 1 versions'
-					],
-					map: true
-				},
-				files: {
-					'dist/Milligrid.css': 'dist/Milligrid.css',
-					'dist/MilligridExtended.css': 'dist/MilligridExtended.css'
-				}
-			}
-		}
-	});
+        // Parse CSS and add vendor-prefixed CSS properties using the Can I Use database.
+        autoprefixer: {
+            default: {
+                options: {
+                    browsers: [
+                        'last 1 versions'
+                    ],
+                    map: true
+                },
+                files: {
+                    'dist/Milligrid.css': 'dist/Milligrid.css',
+                    'dist/MilligridExtended.css': 'dist/MilligridExtended.css'
+                }
+            }
+        }
+    });
 
-	// Default task
-	grunt.registerTask( 'default', [
-		'build',
-		'watch'
-	]);
+    // Default task
+    grunt.registerTask( 'default', [
+        'build',
+        'watch'
+    ]);
 
-	// Build task
-	grunt.registerTask( 'build', [
-		'clean',
-		'sass',
-		'autoprefixer'
-	]);
+    // Build task
+    grunt.registerTask( 'build', [
+        'clean',
+        'sass',
+        'autoprefixer'
+    ]);
 
-	// Automatically loading Grunt tasks
-	require( 'load-grunt-tasks' )( grunt );
+    // Automatically loading Grunt tasks
+    require( 'load-grunt-tasks' )( grunt );
 
-	// Display the elapsed execution time of Grunt tasks
-	require( 'time-grunt' )( grunt );
+    // Display the elapsed execution time of Grunt tasks
+    require( 'time-grunt' )( grunt );
 };
